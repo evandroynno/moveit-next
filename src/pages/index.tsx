596d6b2f4179e03efc9c4e9f9041a443c7c1fc28
evandroynno <evandro.ynno@gmail.com>
2021-03-07 19:36:43 -0300

    criando challengebox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges'
+import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 
@@ -7,14 +10,19 @@ import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
-
       <section>
         <div>
           <Profile/>
           <CompletedChallenges/>
+          <Countdown/>
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
